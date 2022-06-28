6929de541b81539031122fa1ebf17bfaa1347827
Mike van Eckendonk <mike.e@vecks.com.br>
2022-06-28 05:51:09 -0300

    fixed small bugs, add remove book function

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # books-website
+
+# Project Name : Awsome Books ♻️
+
+This is  Pair to Pair project to help you find and remember the best books to read.
+
+# Screenshots
+- Desktop Layout
+
+![Desktop_Layout](assets/img/desktop.png)
+
+- Mobile Layout
+
+![Mobile_Layout](assets/img/mobile.png)
+
+
+
+Additional description about the project and its features.
+
+## Built With
+
+- HTML 
+- CSS
+- Javascript
+- VScode
+- Git & GitHub
+
+## Live Demo link
+
+[Live Demo Link](https://PinkMoon25.github.io/books-website/)
+
+
+## Getting Started
+
+**This is an example of how you may give instructions on setting up your project locally.**
+**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
+
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+have a working git on your localmachine.
+## Install
+To get a working local copy up and running follow these simple steps
+- open terminal
+- clone this project with the command:
+
+
+```
+$ git clone https://github.com/PinkMoon25/books-website.git
+```
+- Finally, run the next command in the terminal to see the web app in your local browser, or also, you can use Live Server in Visual Studio Code.
+```
+$ npm start
+```
+### Usage
+For anyone that whats to learn html & css & Js
+### Run tests
+N/A
+### Deployment
+N/A
+
+
+## Authors
+
+👤 **Author 1 **
+
+- GitHub: [@PinkMoon25](https://github.com/PinkMoon25/)
+- Twitter: [@twitterhandle](https://twitter.com/---)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/---/)
+
+👤 **Author 2 **
+
+- GitHub: [@mrEckendonk](https://github.com/mrEckendonk)
+- Twitter: [@twitter](https://twitter.com/mike_eckendonk)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/mike-van-eckendonk)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/PinkMoon25/books-website/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration 💘
+- Microverse program ⚡
+- My standup team 🏹
+- My family's support 🙌
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- script.js
+++ script.js
@@ -3,59 +3,82 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('add');
 const div = document.querySelector('.book');
+const removeBtn = document.getElementById('remove');
+
 
 const bookArr = [];
 
-function addBooks(){
+function addBooks() {
   let myBooks = {
     title: title.value,
     author: author.value,
     id: bookArr.length + 1
   }
-  bookArr.push(myBooks);
+  if (title.value === '' || author.value === '') {
+    return;
+  } else {
+    bookArr.push(myBooks);
+  }
 };
 
-function storeData(){
+function storeData() {
   localStorage.setItem('bookCollection', JSON.stringify(bookArr));
 }
 
 let data;
 
-function getData(){
+function getData() {
 
   data = JSON.parse(localStorage.getItem('bookCollection'));
 
-  if(data == null) return;
-  
+  if (data == null) return;
+
 }
 
-function displayBooks(){
-  div.innerHTML = '';
-
-  for(let i=0; i<data.length; i++){
-
-  let h2 = document.createElement('h2');
-  let h3 = document.createElement('h3');
-  let removeBtn = document.createElement('button');
-
-  h2.classList.add('title');
-  h3.classList.add('author');
-  removeBtn.classList.add('remove');
-
-  h2.textContent = data[i].title;
-  h3.textContent = data[i].author;
-  removeBtn.textContent = 'remove';
-
-  div.appendChild(h2);
-  div.appendChild(h3);
-  div.appendChild(removeBtn);
-
-  title.value = '';
-  author.value = '';
+function removeBook() {
+  let removeBtn = document.querySelectorAll('.remove');
+  if (data == null) {
+    return;
+  } else {
+    for (let i = 0; i < removeBtn.length; i++) {
+      removeBtn[i].addEventListener('click', function () {
+        let id = this.parentElement.id;
+        data.splice(id, 1);
+      }
+      )
+    }
   }
 }
 
-addBtn.addEventListener('click', (e)=>{
+function displayBooks() {
+  div.innerHTML = '';
+
+  for (let i = 0; i < data.length; i++) {
+
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+    let removeBtn = document.createElement('button');
+    let hr = document.createElement('hr');
+
+    h2.classList.add('title');
+    h3.classList.add('author');
+    removeBtn.classList.add('remove');
+
+    h2.textContent = data[i].title;
+    h3.textContent = data[i].author;
+    removeBtn.textContent = 'remove';
+
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(removeBtn);
+    div.appendChild(hr);
+
+    title.value = '';
+    author.value = '';
+  }
+}
+
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addBooks();
   storeData();
@@ -63,7 +86,16 @@ addBtn.addEventListener('click', (e)=>{
   displayBooks();
 });
 
-document.addEventListener('DOMContentLoaded', ()=>{
+
+
+removeBtn.addEventListener('click', () => {
+  removeBook();
+  storeData();
+  getData();
+  displayBooks();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
   getData();
   displayBooks();
 });
