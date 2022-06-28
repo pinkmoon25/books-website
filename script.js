180eea50f53580b2e3c15221e42b516fdee5f155
Mike van Eckendonk <mike.e@vecks.com.br>
2022-06-28 10:15:19 -0300

    Add final touches

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('add');
 const div = document.querySelector('.book');
-const removeBtn = document.getElementById('remove');
+// const removeBtn = document.getElementById('remove');
 
 
-const bookArr = [];
+let bookArr = [];
 
 function addBooks() {
   let myBooks = {
@@ -25,35 +25,36 @@ function storeData() {
   localStorage.setItem('bookCollection', JSON.stringify(bookArr));
 }
 
-let data;
-
 function getData() {
 
-  data = JSON.parse(localStorage.getItem('bookCollection'));
+  bookArr = JSON.parse(localStorage.getItem('bookCollection'));
 
-  if (data == null) return;
+  console.log(bookArr);
+
+  if (bookArr == null) return;
 
 }
 
-function removeBook() {
-  let removeBtn = document.querySelectorAll('.remove');
-  if (data == null) {
+function removeBook(id) {
+ /*  let removeBtn = document.querySelectorAll('.remove');
+  if (bookArr == null) {
     return;
   } else {
     for (let i = 0; i < removeBtn.length; i++) {
       removeBtn[i].addEventListener('click', function () {
         let id = this.parentElement.id;
-        data.splice(id, 1);
+        bookArr.splice(id, 1);
       }
       )
     }
-  }
+  } */
+  bookArr.splice(id, 1);
 }
 
 function displayBooks() {
   div.innerHTML = '';
 
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < bookArr.length; i++) {
 
     let h2 = document.createElement('h2');
     let h3 = document.createElement('h3');
@@ -64,9 +65,16 @@ function displayBooks() {
     h3.classList.add('author');
     removeBtn.classList.add('remove');
 
-    h2.textContent = data[i].title;
-    h3.textContent = data[i].author;
+    h2.textContent = bookArr[i].title;
+    h3.textContent = bookArr[i].author;
     removeBtn.textContent = 'remove';
+    removeBtn.addEventListener('click', () => {
+      console.log('removed');
+      removeBook(i);
+      storeData();
+      getData();
+      displayBooks();
+    })
 
     div.appendChild(h2);
     div.appendChild(h3);
@@ -86,18 +94,21 @@ addBtn.addEventListener('click', (e) => {
   displayBooks();
 });
 
+/* if (removeBtn) {
+  removeBtn.addEventListener('click', () => {
+    console.log('removed');
+    removeBook();
+    storeData();
+    getData();
+    displayBooks();
+  });
+} */
 
-
-removeBtn.addEventListener('click', () => {
-  removeBook();
-  storeData();
+window.addEventListener('load', () => {
   getData();
   displayBooks();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  getData();
-  displayBooks();
-});
+console.log('working');
 
 
