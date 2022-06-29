d5534ce114bee6a95fd05665db93f0e833660384
Tushar <singhtushar348@gmail.com>
2022-06-29 15:47:05 +0100

    Refactor script to include Classes

--- script.js
+++ script.js
@@ -1,75 +1,89 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('add');
-const div = document.querySelector('.book');
+const bookList = document.querySelector('.book');
 
-let bookArr = [];
-
-function addBooks() {
-  const myBooks = {
-    title: title.value,
-    author: author.value,
-    id: bookArr.length + 1,
-  };
-  if (title.value === '' || author.value === '') { return; }
-
-  bookArr.push(myBooks);
-}
-
-function storeData() {
-  localStorage.setItem('bookCollection', JSON.stringify(bookArr));
-}
-
-function getData() {
-  bookArr = JSON.parse(localStorage.getItem('bookCollection')) || [];
-}
-
-function removeBook(id) {
-  bookArr.splice(id, 1);
-}
-
-function displayBooks() {
-  div.innerHTML = '';
-
-  for (let i = 0; i < bookArr.length; i += 1) {
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
-    const removeBtn = document.createElement('button');
-    const hr = document.createElement('hr');
-
-    h2.classList.add('title');
-    h3.classList.add('author');
-    removeBtn.classList.add('remove');
-
-    h2.textContent = bookArr[i].title;
-    h3.textContent = bookArr[i].author;
-    removeBtn.textContent = 'remove';
-    removeBtn.addEventListener('click', () => {
-      removeBook(i);
-      storeData();
-      getData();
-      displayBooks();
-    });
-
-    div.appendChild(h2);
-    div.appendChild(h3);
-    div.appendChild(removeBtn);
-    div.appendChild(hr);
-
-    title.value = '';
-    author.value = '';
+class Book{
+  constructor(title, author, id){
+    this.title = title;
+    this.author = author;
+    this.id = id;
   }
 }
 
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  addBooks();
-  storeData();
-  getData();
-  displayBooks();
-});
+class BookData{
 
-window.addEventListener('load', () => {
-  getData();
-  displayBooks();
-});
+  static addBook(book){
+    const div = document.createElement('div');
+    const para = document.createElement('p');
+    const removeBtn = document.createElement('button');
+
+    div.classList.add('new-book');
+    para.classList.add('book-detail');
+    removeBtn.classList.add('remove');
+
+    para.innerHTML = `<q><cite>${book.title}</cite></q> by ${book.author}`;
+    removeBtn.setAttribute('id', `${book.id}`);
+    removeBtn.textContent = 'remove';
+    removeBtn.addEventListener('click', () => {
+      Storage.removeBook(book.id);
+      removeBtn.parentElement.remove();
+    })
+
+    div.appendChild(para);
+    div.appendChild(removeBtn);
+
+    bookList.appendChild(div);
+  }
+
+  static displayBooks(){
+    const books = Storage.getBook();
+    books.forEach((book) => {
+      BookData.addBook(book);
+    })
+  }
+}
+
+class Storage{
+  static getBook(){
+    let bookArr = [];
+    if(localStorage.getItem('bookCollection') !== null) {
+        bookArr = JSON.parse(localStorage.getItem('bookCollection'));
+    }
+    else {
+      bookArr = [];
+    }
+  
+    return bookArr;
+  }
+
+  static storeBook(book){
+    const books = Storage.getBook();
+    books.push(book);
+    localStorage.setItem('bookCollection', JSON.stringify(books));
+  }
+  
+  static removeBook(id){
+    let book = Storage.getBook();
+    book.splice(id, 1);
+    localStorage.setItem('bookCollection', JSON.stringify(book));
+  }
+}
+
+addBtn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  if(title.value === '' || author.value === '') return;
+  else{
+    const bookArr = Storage.getBook();
+    const newBook = new Book(title.value, author.value, bookArr.length);
+    BookData.addBook(newBook);
+    Storage.storeBook(newBook);
+    title.value = '';
+    author.value = '';
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  Storage.getBook();
+  BookData.displayBooks();
+} )
