077808f4b65704e8d63126fbd922cebb79b0da90
Mike van Eckendonk <mike.e@vecks.com.br>
2022-06-29 13:49:27 -0300

    final touches

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const author = document.getElementById('author');
 const addBtn = document.getElementById('add');
 const bookList = document.querySelector('.book');
 
-class Book{
-  constructor(title, author, id){
+class Book {
+  constructor(title, author, id) {
     this.title = title;
     this.author = author;
     this.id = id;
   }
 }
 
-class BookData{
-
-  static addBook(book){
+class BookData {
+  static addBook(book) {
     const div = document.createElement('div');
     const para = document.createElement('p');
     const removeBtn = document.createElement('button');
@@ -28,7 +27,7 @@ class BookData{
     removeBtn.addEventListener('click', () => {
       Storage.removeBook(book.id);
       removeBtn.parentElement.remove();
-    })
+    });
 
     div.appendChild(para);
     div.appendChild(removeBtn);
@@ -36,54 +35,52 @@ class BookData{
     bookList.appendChild(div);
   }
 
-  static displayBooks(){
+  static displayBooks() {
     const books = Storage.getBook();
     books.forEach((book) => {
       BookData.addBook(book);
-    })
+    });
   }
 }
 
-class Storage{
-  static getBook(){
+class Storage {
+  static getBook() {
     let bookArr = [];
-    if(localStorage.getItem('bookCollection') !== null) {
-        bookArr = JSON.parse(localStorage.getItem('bookCollection'));
-    }
-    else {
+    if (localStorage.getItem('bookCollection') !== null) {
+      bookArr = JSON.parse(localStorage.getItem('bookCollection'));
+    } else {
       bookArr = [];
     }
-  
+
     return bookArr;
   }
 
-  static storeBook(book){
+  static storeBook(book) {
     const books = Storage.getBook();
     books.push(book);
     localStorage.setItem('bookCollection', JSON.stringify(books));
   }
-  
-  static removeBook(id){
-    let book = Storage.getBook();
+
+  static removeBook(id) {
+    const book = Storage.getBook();
     book.splice(id, 1);
     localStorage.setItem('bookCollection', JSON.stringify(book));
   }
 }
 
-addBtn.addEventListener('click', (e)=>{
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  if(title.value === '' || author.value === '') return;
-  else{
-    const bookArr = Storage.getBook();
-    const newBook = new Book(title.value, author.value, bookArr.length);
-    BookData.addBook(newBook);
-    Storage.storeBook(newBook);
-    title.value = '';
-    author.value = '';
-  }
-})
+  if (title.value === '' || author.value === '') return;
+
+  const bookArr = Storage.getBook();
+  const newBook = new Book(title.value, author.value, bookArr.length);
+  BookData.addBook(newBook);
+  Storage.storeBook(newBook);
+  title.value = '';
+  author.value = '';
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   Storage.getBook();
   BookData.displayBooks();
-} )
+});
